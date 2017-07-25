fe445cddd4fde9acdc87e7e7b3fbc356d1c586aa
Thea <ywgao112@gmail.com>
2017-07-25 10:36:28 +0800

    final version of 3 model----DNN,CNN,RNN(LSTM)

--- kerasLSTM.py
+++ kerasLSTM.py
@@ -1,70 +1,77 @@
-from keras.callbacks import EarlyStopping
-from sklearn.preprocessing import LabelEncoder
-from sklearnModel import get_accuracy
-from dataprocessing import get_data
-import numpy
-from keras.datasets import imdb
-from keras.models import Sequential, model_from_json
-from keras.layers import Dense
-from keras.layers import LSTM
-from keras.layers.embeddings import Embedding
-from keras.preprocessing import sequence
 import os
 
+import numpy
+from keras.callbacks import EarlyStopping
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.models import Sequential, model_from_json
 
-numpy.random.seed(24)
-trainX,trainY,testX,testY,validX,validY=get_data()
+# fix random seed for reproducibility
+from preprocessData import getDataXY
 
-def process_data_LSTM(X,Y):
-    finalX=[]
-    finalY=[]
-    for i in range(0,len(X),7):
-        if list(Y[i:i+7]).count('sing')==7:
-            finalX.append(X[i:i+7])
+
+# load dataset
+trainX, trainY, testX, testY, validX, validY = getDataXY()
+
+
+def preDataStep(dataX, dataY):
+    finalX = []
+    finalY = []
+    step = 7
+    for item in range(0, len(dataX), step):
+        if step == list(dataY[item:item + step]).count('sing'):
+            finalX.append(dataX[item:item + step])
             finalY.append(1)
-        elif list(Y[i:i+7]).count('nosing')==7:
-            finalX.append(X[i:i+7])
+
+        elif step == list(dataY[item:item + step]).count('nosing'):
+            finalX.append(dataX[item:item + step])
             finalY.append(0)
-            print i,'second'
-        else:
-            pass
+
+    print 'nosing', finalY.count(0)
+    print 'sing', finalY.count(1)
+
     finalX = numpy.array(finalX)
     finalY = numpy.array(finalY)
-    return finalX,finalY
+
+    return finalX, finalY
 
 
-trainX,trainY=process_data_LSTM(trainX[:100000],trainY[:100000])
-validX,validY=process_data_LSTM(validX[:10000],validY[:10000])
-testX,testY=process_data_LSTM(testX[:10000],testY[:10000])
-print trainY
+trainX, trainY = preDataStep(trainX[:100000], trainY[:100000])
+validX, validY = preDataStep(validX, validY)
+testX, testY = preDataStep(testX, testY)
 
+# create the model
+model_path = 'lstm.josn'
+model_weights = 'lstm.h5'
+retrain = 1
 
-def create_model():
+if os.path.isfile(model_path) and not retrain == 1:
+    # load json and create model
+    json_file = open(model_path, 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    model = model_from_json(loaded_model_json)
+    # load weights into new model
+    model.load_weights(model_weights)
+    print("Loaded model from disk")
+else:
+    numpy.random.seed(24)
     model = Sequential()
-    model.add(LSTM(100,input_shape=(7,13)))
+    model.add(LSTM(100, input_shape=(7, 13), return_sequences=True))
+    model.add(LSTM(25))
     model.add(Dense(1, activation='sigmoid'))
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     print(model.summary())
-    return model
-if os.path.isfile('kerasLSTM.json'):
-    json_file=open('kerasLSTM.json','r')
-    loaded_model_json=json_file.read()
-    json_file.close()
-    model=model_from_json(loaded_model_json)
-    model.load_weights('kerasLSTM.h5')
-    print("Saved model to disk")
-else:
-    model=create_model()    
-    model.fit(trainX,trainY,nb_epoch=10,batch_size=32)
-
-if not os.path.isfile('kerasLSTM.json'):
-    model_json = model.to_json()
-    with open("kerasLSTM.json", "w") as json_file:
+    callbacks = [EarlyStopping(monitor='val_loss', patience=2, verbose=0)]
+    model.fit(trainX, trainY, validation_data=(validX, validY), batch_size=64, nb_epoch=10,
+              callbacks=callbacks)
+    if not os.path.isfile(model_path) or retrain == 1:
+        model_json = model.to_json()
+    with open(model_path, "w") as json_file:
         json_file.write(model_json)
-    model.save_weights('kerasLSTM.h5')
-
-
-
-predict_Y=model.predict(validX)
-scores = model.evaluate(validX, validY, verbose=0)
-print("Accuracy: %.2f%%" % (scores[1]*100))
+    # serialize weights to HDF5
+    model.save_weights(model_weights)
+    print("Saved model to disk")
+# Final evaluation of the model
+scores = model.evaluate(testX, testY, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1] * 100))
